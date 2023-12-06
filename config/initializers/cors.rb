5cd0df69c85c2a811e00b3a58e930d4d8c046d19
SlaBrutte <samlabrutte@yahoo.com>
2023-12-06 15:30:28 -0800

    backend structure sl

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+# Allows us to accept POST, PUT, and DELETE requests in our server side application.
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'  # <- change this to allow requests from any domain while in development.
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
