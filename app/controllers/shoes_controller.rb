bb9a90579da5bed8f2bf832924eaef5455bbb9d1
Joseph Kong <josephk1018@gmail.com>
2023-12-07 14:17:52 -0800

    api enpoints completed

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,30 @@
 class ShoesController < ApplicationController
+    def index 
+        shoes = Shoe.all 
+        render json:shoes
+    end
+
+    def show
+    end
+
+    def create
+        shoe = Shoe.create(shoe_params)
+        render json: shoe
+    end
+
+    def update 
+        shoe = Shoe.find(params[:id])
+        shoe.update(shoe_params)
+        render json: shoe
+    end
+
+    def destroy
+        shoe = Shoe.find(params[:id])
+        shoe.destroy
+        render json: shoe
+    end
+    private
+    def shoe_params 
+    params.require(:shoe).permit(:name, :age, :enjoys, :image)
+    end
 end
